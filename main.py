73dcab905b471ef746af98f9034914396172a402
vinhle000 <vinhle000@gmail.com>
2024-11-21 17:53:56 -0800

    implemnted astroid field

--- main.py
+++ main.py
@@ -4,6 +4,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     print("Starting asteroids!")
@@ -20,10 +22,15 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
 
     Player.containers = (updatable, drawable)
-    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT /2 )
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable)
 
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT /2 )
+    asteroid_field = AsteroidField()
 
     while True:
         for event in pygame.event.get():
@@ -32,15 +39,11 @@ def main():
 
         pygame.Surface.fill(screen, (000, 000, 000),)
 
-        player.update(dt)
-        player.draw(screen)
-
         for sprite in  updatable:
             sprite.update(dt)
 
         for sprite in drawable:
             sprite.draw(screen)
-
 
 
 
