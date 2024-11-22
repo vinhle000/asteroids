e4bb64c990207cb41e87b6e9f78614715af92c9e
vinhle000 <vinhle000@gmail.com>
2024-11-21 16:40:11 -0800

    added player move method and added Player class to updatable and drawable groups

--- main.py
+++ main.py
@@ -18,6 +18,10 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT /2 )
 
 
@@ -30,6 +34,12 @@ def main():
 
         player.update(dt)
         player.draw(screen)
+
+        for sprite in  updatable:
+            sprite.update(dt)
+
+        for sprite in drawable:
+            sprite.draw(screen)
 
 
 
--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 import pygame
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
 from circleshape import CircleShape
 
 class Player(CircleShape):
@@ -32,3 +32,13 @@ class Player(CircleShape):
         if keys[pygame.K_d]:
              # ?
             self.rotate(dt * -1)
+
+        if keys[pygame.K_w]:
+            self.move(dt)
+
+        if keys[pygame.K_s]:
+            self.move(dt * -1)
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
